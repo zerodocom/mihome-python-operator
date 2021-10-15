dd39f5d6e8e98634d60513e04c9c4993a4d89ec1
zerodo.com <zerodo.com@gmail.com>
2021-10-16 06:37:59 +0800

    支持读取kubeconfig中的路径证书

--- operator/aiokube.py
+++ operator/aiokube.py
@@ -34,50 +34,61 @@ class Kube:
         if user.get("token"):
             self.headers["Authorization"] = f"Bearer {user['token']}"
         else:
+            
+            if user.get('client-certificate-data') is not None:
+                client_certificate_file = generate_temp_file(
+                    base64.b64decode(user["client-certificate-data"]).decode('utf-8')
+                ).name
+            elif user.get('client-certificate') is not None:
+                client_certificate_file = user["client-certificate"]
+            else:
+                raise ValueError("client-certificate-data or client-certificate is required")
+
+            if user.get('client-key-data') is not None:
+                client_key_file = generate_temp_file(
+                    base64.b64decode(user["client-key-data"]).decode('utf-8')
+                ).name
+            elif user.get('client-key') is not None:
+                client_key_file = user["client-key"]
+            else:
+                raise ValueError("client-key-data or client-key is required")
+
+           
+            if user.get('certificate-authority-data') is not None:
+                certificate_authority_file = generate_temp_file(
+                    base64.b64decode(user["certificate-authority-data"]).decode('utf-8')
+                ).name
+            elif user.get('certificate-authority') is not None:
+                certificate_authority_file = user["certificate-authority"]
+            else:
+                certificate_authority_file = None
+ 
             self.ssl = self._init_ssl(
-                client_certificate_data=user.get('client-certificate-data'),
-                client_key_data=user.get('client-key-data'),
-                certificate_authority_data=cluster.get('certificate-authority-data')
+                client_certificate_file=client_certificate_file,
+                client_key_file=client_key_file,
+                cafile=certificate_authority_file
             )
+
+            if user.get('client-certificate-data') is not None:
+                os.remove(client_certificate_file)
+            if user.get('certificate-authority-data') is not None:
+                os.remove(certificate_authority_file)
+            if user.get('client-key-data') is not None:
+                os.remove(client_key_file)
 
 
     @staticmethod
-    def _init_ssl(client_certificate_data, client_key_data, certificate_authority_data=None):
+    def _init_ssl(client_certificate_file, client_key_file, cafile=None):
         """
         generate sslcontext by cert info
         """
-        certificate_authority = None
-        if certificate_authority_data is not None:
-            certificate_authority = tempfile.NamedTemporaryFile(delete=False)
-            with open(certificate_authority.name, 'w') as f:
-                f.write(base64.b64decode(certificate_authority_data).decode('utf-8'))
-            cafile = certificate_authority.name
-        else:
-            cafile = None
-
-        if client_certificate_data is None:
-            raise ValueError("client_certificate_data is required")
-        client_certificate = tempfile.NamedTemporaryFile(delete=False)
-        with open(client_certificate.name, 'w') as f:
-            f.write(base64.b64decode(client_certificate_data).decode('utf-8'))
-
-        if client_key_data is None:
-            raise ValueError("client_key_data is required")
-        client_key = tempfile.NamedTemporaryFile(delete=False)
-        with open(client_key.name, 'w') as f:
-            f.write(base64.b64decode(client_key_data).decode('utf-8'))
 
         sslcontext = ssl.create_default_context(cafile=cafile)
-        if certificate_authority_data is None:
+        if cafile is None:
             sslcontext.check_hostname = False
             sslcontext.verify_mode = ssl.CERT_NONE
 
-        sslcontext.load_cert_chain(certfile=client_certificate.name, keyfile=client_key.name)
-
-        if certificate_authority:
-            os.remove(certificate_authority.name)
-        os.remove(client_certificate.name)
-        os.remove(client_key.name)
+        sslcontext.load_cert_chain(certfile=client_certificate_file, keyfile=client_key_file)
 
         return sslcontext
 
@@ -93,6 +104,13 @@ class Kube:
             return self.settings.get(key)
         else:
             return overwrite_value
+
+    @staticmethod
+    def generate_temp_file(raw):
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        with open(temp_file.name, 'w') as f:
+            f.write(raw)
+        return temp_file
 
     async def get(self, uri, params=None, timeout=None):
         async with self.get_session().get(
